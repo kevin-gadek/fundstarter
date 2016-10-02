4e0217edea4a27b8abf98bde879eb8f33b7d33cc
Ubuntu <ubuntu@ip-172-31-7-214.us-west-1.compute.internal>
2016-10-02 18:32:26 +0000

    part 2

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var fs = require('fs');
 var http = require('http');
 var port = Number(process.env.PORT || 8080);
-var data = fs.readFileSync('public/index.html');
+var requestListener = function(req, res)
+{
+	fs.open('public/index.html', 'r', function(err, fd){
+		if(err) return console.log(err);
 
-var server = http.createServer(function(req, res){
-	res.writeHead(200, {"Content-Type": "text/html"});
-	res.end(data);
-	});
-server.listen(8080);
+		var buffer = new Buffer(2500);
+		fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer)
+		{
+			var data = buffer.toString('utf8');
+			res.writeHeader(200, {'Content-Type': 'text/html'});
+			res.write(data);
+			res.end();
+		}); //end read
+	}); 
+
+};
+
+
+var server = http.createServer(requestListener);
+server.listen(port);
