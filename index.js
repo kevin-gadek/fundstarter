6fe07de893c9d70eb1cc6f86f2c3a0edadce9d98
Ubuntu <ubuntu@ip-172-31-7-214.us-west-1.compute.internal>
2016-10-02 17:44:22 +0000

    make it even better

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var port = Number(process.env.PORT || 8080);
 
 http.createServer(function(req, res){
-	fs.readFile('index.html', function (err, data){
+	fs.readFile('public/index.html', function (err, data){
 		res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': data.length});
 		res.write(data);
 		res.end();
